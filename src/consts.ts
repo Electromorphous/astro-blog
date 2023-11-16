98d4519e10c383b86e02157ea5f292363c725574
Electromorphous <pstation.474@gmail.com>
2023-11-16 15:56:32 +0530

    update styling and site url

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Website metadata
-export const SITE_URL: string = "https://astrostarter.zank.studio";
+export const SITE_URL: string = "https://portalequatorial.com.br";
 export const SITE_TITLE: string = "Potrtal Equatorial";
 export const SITE_DESCRIPTION: string = "Welcome to my website!";
 
